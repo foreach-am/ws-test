d7ad7582b9ab06f6f2e23bb6e2ba9e6fd6908f74
rado196 <rado196b@gmail.com>
2022-12-29 12:04:43 +0400

    fixes

--- src/client.js
+++ src/client.js
@@ -1,13 +1,18 @@
 const WebSocketClient = require("ws");
+const crypto = require("crypto");
 
 const url = "ws://185.193.67.250:4506/websocket-example";
 const ws = new WebSocketClient(url);
+
+function sendHeartbeat() {
+  ws.send([2, crypto.randomUUID(), 'Heartbeat', {}]);
+}
 
 let _interval = null;
 const heartbeat = {
   start: function () {
     _interval = setInterval(function () {
-      // ...
+      sendHeartbeat();
     }, 15_000);
   },
   stop: function () {
